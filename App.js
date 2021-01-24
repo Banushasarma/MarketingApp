d2a8ffd3d087df30d9caca93a90e91e8b4632986
Banushasarma <32017922+Banushasarma@users.noreply.github.com>
2021-01-24 22:27:37 +0530

    Lists chapter completion

--- App.js
+++ App.js
@@ -7,11 +7,24 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card'
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/screens/Screen';
+import Icon from './app/components/Icon';
+import ListItem from './app/components/ListItem';
+import AccountScreen from './app/screens/AccountScreen';
+import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
   // return <WelcomeScreen />
-  return <ViewImageScreen />
+  // return <ViewImageScreen />
   // return <ListingDetailsScreen />
+  // return <MessagesScreen />
+
+  // return <Screen>
+  //   <ListItem title="My title" ImageComponent={<Icon name="email" />} />
+  // </Screen>
+  // return <AccountScreen />
+  return <ListingScreen />
 }
 
 const styles = StyleSheet.create({
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,10 +1,69 @@
-import React from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { FlatList, StyleSheet, View } from 'react-native'
+
+import ListItem from '../components/ListItem'
+import ListItemDeleteAction from '../components/ListItemDeleteAction'
+import ListItemSeparator from '../components/ListItemSeparator'
+import Screen from './Screen'
+
+
+const initialMessages = [
+    {
+        id: 1,
+        title: 'T1',
+        description: 'D1',
+        image: require('../assets/mosh.jpg')
+    },
+    {
+        id: 2,
+        title: 'T2',
+        description: 'D2',
+        image: require('../assets/mosh.jpg')
+    }
+]
 
 export default function MessagesScreen() {
+
+    const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const handleDelete = message => {
+        //Delete the message from messages
+        const newMesssages = messages.filter(m => m.id !== message.id)
+        setMessages(newMesssages)
+    }
+
     return (
-        <FlatList />
+        <Screen>
+            <FlatList
+                data={messages}
+                keyExtractor={message => message.id.toString()}
+                renderItem={({ item }) =>
+                    <ListItem
+                        title={item.title}
+                        subTitle={item.description}
+                        image={item.image}
+                        onPress={() => console.log("Message selected", item)}
+                        renderRightActions={() => <ListItemDeleteAction onPress={() => handleDelete(item)} />}
+                    />}
+                ItemSeparatorComponent={ListItemSeparator}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setMessages([
+                        {
+                            id: 2,
+                            title: 'T2',
+                            description: 'D2',
+                            image: require('../assets/mosh.jpg')
+                        }
+                    ])
+                }}
+            />
+
+        </Screen>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
